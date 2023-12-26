69534826b384cc1c60e9c18447da73a6c607b4a7
Anouar <a.benlaqlib.com>
2023-12-27 00:18:41 +0100

    models.py updated screenshots added .

--- coreApp/models.py
+++ coreApp/models.py
@@ -86,3 +86,134 @@ class Fournisseur(models.Model):
 
     def __str__(self):
         return self.raison_sociale
+    
+class Prospect(models.Model):
+    entreprise = models.ForeignKey(Entreprise, on_delete=models.CASCADE)
+    TYPE = [('Societe', 'Societé'),('Particulier', 'Particulier'),]
+    type_societe = models.CharField(max_length=20,choices=TYPE,default='Societe')
+    raison_sociale = models.CharField(max_length=255, verbose_name="Raison sociale")
+    telephone = models.CharField(max_length=20, verbose_name="Téléphone")
+    adresse = models.TextField(verbose_name="Adresse")
+    ville = models.CharField(max_length=255, verbose_name="Ville")
+    code_postal = models.CharField(max_length=10, verbose_name="Code postal")
+    pays= models.CharField(max_length=255, verbose_name="Pays")
+    telephone = models.CharField(max_length=20, verbose_name="Téléphone")
+    gsm = models.CharField(max_length=20, verbose_name="GSM")
+    fax = models.CharField(max_length=20, verbose_name="Fax")
+    email = models.EmailField(verbose_name="Adresse email")
+    site_web = models.URLField(max_length=200, blank=True, null=True)
+    
+    banque = models.CharField(max_length=255, verbose_name="Banque")
+    RIB= models.CharField(max_length=27, verbose_name="RIB")
+    RC=models.CharField(max_length=27, verbose_name="RC- Registre de Commerce")
+    devise = models.CharField(max_length=3, default='MAD', verbose_name="Devise")
+    ICE = models.CharField(max_length=20)
+    IF = models.CharField(max_length=20,verbose_name="Identifiant Fiscal")
+    solde_initial = models.DecimalField(max_digits=10, decimal_places=2)
+    plafond = models.DecimalField(max_digits=10, decimal_places=2)
+    condition_de_reglement = models.CharField(max_length=20,default="à la livraison")
+    date_initial_solde = models.DateField()
+
+    def __str__(self):
+        return self.raison_sociale
+    
+class Commercial(models.Model):
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    telephone = models.CharField(max_length=15, unique=True)
+    email = models.EmailField(unique=True)
+    ville = models.CharField(max_length=50, null=True, blank=True)
+    commission_pourcentage = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+
+    def __str__(self):
+        return f"{self.prenom} {self.nom}"
+    
+class Categorie(models.Model):
+    nom = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nom
+
+class SousCategorie(models.Model):
+    nom = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nom
+    
+class Produit(models.Model):
+    entreprise = models.ForeignKey(Entreprise, on_delete=models.CASCADE, related_name='produits')
+    reference = models.CharField(max_length=50, unique=True)
+    code_barre = models.CharField(max_length=50, unique=True)
+    designation = models.CharField(max_length=255)
+    pays_dorigine = models.CharField(max_length=255)
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+    sous_categorie = models.ForeignKey(SousCategorie, on_delete=models.CASCADE)
+    stock = models.PositiveIntegerField(default=0)
+    emplacement = models.CharField(max_length=50, null=True, blank=True)
+    sur_catalog = models.BooleanField(default=False)
+    prix_1 = models.DecimalField(max_digits=10, decimal_places=2)
+    prix_ttc_1 = models.DecimalField(max_digits=10, decimal_places=2)
+    prix_2 = models.DecimalField(max_digits=10, decimal_places=2)
+    prix_ttc_2 = models.DecimalField(max_digits=10, decimal_places=2)
+    prix_3 = models.DecimalField(max_digits=10, decimal_places=2)
+    prix_ttc_3 = models.DecimalField(max_digits=10, decimal_places=2)
+    prix_achat_net = models.DecimalField(max_digits=10, decimal_places=2)
+    prix_achat_net_ttc = models.DecimalField(max_digits=10, decimal_places=2)
+    remise_achat = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    prix_achat_brut = models.DecimalField(max_digits=10, decimal_places=2)
+    prix_achat_brut_ttc = models.DecimalField(max_digits=10, decimal_places=2)
+    image_produit = models.ImageField(upload_to='produit/', null=True, blank=True)
+    tva_pourcentage = models.DecimalField(max_digits=5, decimal_places=2)
+    largeur = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    longueur = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    epaisseur = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    unite_principale = models.CharField(max_length=20, null=True, blank=True)
+    unite_vente = models.CharField(max_length=20, null=True, blank=True)
+    marque = models.CharField(max_length=50, null=True, blank=True)
+    couleur = models.CharField(max_length=50, null=True, blank=True)
+    taille = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.reference} - {self.designation}"
+    
+class Service(models.Model):
+    entreprise = models.ForeignKey(Entreprise, on_delete=models.CASCADE, related_name='services')
+    reference = models.CharField(max_length=50, unique=True)
+    designation = models.CharField(max_length=255)
+    pays_dorigine = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    prix_1 = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    prix_2 = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    prix_3 = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    tva_pourcentage = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    prix_ttc_1 = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    prix_ttc_2 = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    prix_ttc_3 = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    prix_achat_net = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    prix_achat_pourcentage = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    remise_achat = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.reference} - {self.designation}"
+
+class Pack(models.Model):
+    nom = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    produits = models.ManyToManyField('Produit', related_name='packs')
+    
+    def __str__(self):
+        return self.nom
+    
+class Promotion(models.Model):
+    produit = models.ForeignKey('Produit', on_delete=models.CASCADE, null=True, blank=True)
+    pack = models.ForeignKey('Pack', on_delete=models.CASCADE, null=True, blank=True)
+    taux_reduction = models.DecimalField(max_digits=5, decimal_places=2)
+    date_debut = models.DateField()
+    date_fin = models.DateField()
+
+    def __str__(self):
+        return f"Promotion {self.id}"
+
+    class Meta:
+        verbose_name = "Promotion"
+        verbose_name_plural = "Promotions"
