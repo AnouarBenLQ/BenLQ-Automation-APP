a02e7faa71314d08bd202aa0c268b6d580624dd5
Anouar <a.benlaqlib.com>
2024-01-05 08:43:25 +0100

    models updated invoces.

--- coreApp/models.py
+++ coreApp/models.py
@@ -218,27 +218,29 @@ class Promotion(models.Model):
     class Meta:
         verbose_name = "Promotion"
         verbose_name_plural = "Promotions"
-
-class Devis(models.Model):
+        
+class Devis_Vente(models.Model):
     
     STATUS_CHOICES = [
         ('encours', 'En cours'),
-        ('echu', 'Échu'),
+        ('expire', 'Éxpiré'),
         ('confirme', 'Confirmé'),
         ('annule', 'Annulé'),
         ('paye', 'Payé'),
     ]
+    entreprise = models.ForeignKey(Entreprise, on_delete=models.CASCADE, related_name='devis')
     suivi_par = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True, related_name='devis_suivi_par')
     status_devis = models.CharField(max_length=20, choices=STATUS_CHOICES, default='encours')
     date = models.DateTimeField(auto_now_add=True)
+    date_modification =  models.DateTimeField(auto_now=True)
     echeance = models.DateTimeField(auto_now_add=True)
     reference = models.CharField(max_length=50, unique=True)
-    document = models.FileField(upload_to='documents/')
+    document = models.FileField(upload_to='documents/devisdevente')
     montant_total_ttc = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     type_devis = models.CharField(max_length=10, choices=[('achat', 'Achat'), ('vente', 'Vente')])
-    sous_type_devis = models.CharField(max_length=10, choices=[('commande', 'Commande'), ('livraison', 'Livraison'), ('retour', 'Retour'),('facture', 'Facture')])
+    bon_de_commande = models.ForeignKey("Commande", on_delete=models.SET_NULL, null=True, blank=True, related_name='bon_de_commande')
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='devis_clients', null=True, blank=True)
-    fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE, related_name='devis_fournisseurs', null=True, blank=True)
+   
     
     def save(self, *args, **kwargs):
         # Set default value for echeance 1 month later than the date
@@ -255,7 +257,7 @@ class Devis(models.Model):
         return total_ttc
 
 class LigneDevis(models.Model):
-    devis = models.ForeignKey(Devis, on_delete=models.CASCADE, related_name='lignes_devis')
+    devis = models.ForeignKey(Devis_Vente, on_delete=models.CASCADE, related_name='lignes_devis')
     designation = models.CharField(max_length=100)
     quantite = models.PositiveIntegerField(default=1)
     tva = models.DecimalField(max_digits=5, decimal_places=2, default=0.2)
@@ -263,6 +265,8 @@ class LigneDevis(models.Model):
     montant_ht = models.DecimalField(max_digits=10, decimal_places=3)
     montant_ttc = models.DecimalField(max_digits=10, decimal_places=3)
     remise = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+
+
     
     def save(self, *args, **kwargs):
         # Set default value for echeance 1 month later than the date
@@ -274,4 +278,64 @@ class LigneDevis(models.Model):
             self.montant_tva = self.tva * self.montant_ht
 
         super().save(*args, **kwargs)
-        
+
+class Commande(models.Model):
+     
+    STATUS_CHOICES = [
+        ('encours', 'En cours'),
+        ('expire', 'Éxpiré'),
+        ('confirme', 'Confirmé'),
+        ('annule', 'Annulé'),
+        ('paye', 'Payé'),
+    ]
+    entreprise = models.ForeignKey(Entreprise, on_delete=models.CASCADE, related_name='commandes')
+    suivi_par = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True, related_name='devis_suivi_par')
+    status_devis = models.CharField(max_length=20, choices=STATUS_CHOICES, default='encours')
+    date = models.DateTimeField(auto_now_add=True)
+    date_modification =  models.DateTimeField(auto_now=True)
+    echeance = models.DateTimeField(auto_now_add=True)
+    reference = models.CharField(max_length=50, unique=True)
+    document = models.FileField(upload_to='documents/bonsdecommande')
+    montant_total_ttc = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    type_devis = models.CharField(max_length=10, choices=[('achat', 'Achat'), ('vente', 'Vente')])
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='devis_clients', null=True, blank=True)
+    bon_de_livraison = models.ForeignKey('BonDeLivraison', on_delete=models.SET_NULL, null=True, blank=True, related_name='bon_de_livraison')
+
+   
+    
+    def save(self, *args, **kwargs):
+        # Set default value for echeance 1 month later than the date
+        if not self.echeance:
+            self.echeance = self.date + timezone.timedelta(days=30)
+        self.montant_total_ttc = self.calculate_montant_total_ttc()
+
+        super().save(*args, **kwargs)
+    
+    def calculate_montant_total_ttc(self):
+        # Calculate the total TTC amount based on all related LigneDevis instances
+        lignes_devis = self.lignes_devis.all()
+        total_ttc = sum(ligne.montant_ttc for ligne in lignes_devis)
+        return total_ttc
+    
+class LigneCommande(models.Model):
+    commande = models.ForeignKey("Commande", on_delete=models.CASCADE, related_name='lignes_commandes')
+    designation = models.CharField(max_length=100)
+    quantite = models.PositiveIntegerField(default=1)
+    tva = models.DecimalField(max_digits=5, decimal_places=2, default=0.2)
+    montant_tva = models.DecimalField(max_digits=10, decimal_places=3)
+    montant_ht = models.DecimalField(max_digits=10, decimal_places=3)
+    montant_ttc = models.DecimalField(max_digits=10, decimal_places=3)
+    remise = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    def save(self, *args, **kwargs):
+        # Set default value for echeance 1 month later than the date
+        if self.montant_ttc is None:
+            self.montant_ttc = self.montant_ht * (1 + self.tva)  # Calculate montant_ttc if not provided
+        if self.montant_total_ttc is None:
+            self.montant_total_ttc = self.quantite * self.montant_ttc
+        if self.montant_tva is None:
+            self.montant_tva = self.tva * self.montant_ht
+
+        super().save(*args, **kwargs)
+
+class Livraison(models.Model):
+    
