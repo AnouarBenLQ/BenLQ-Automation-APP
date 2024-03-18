6573a3d2de12cfe275c3d99fd1a03ed90b48f4c3
Anouar <a.benlaqlib.com>
2024-03-18 00:56:14 +0000

    invoice model refactored

--- transactions/models.py
+++ transactions/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from coreApp.models import Entreprise
 from accounts.models import CustomUser
-from contacts.models import Client,Commercial
-from productsandservices.models import Remise
+from contacts.models import Client,Commercial,ConditionsReglement
+from productsandservices.models import Remise,Produit
 
 # Create your models here.*
 class BaseDevis(models.Model):
@@ -23,10 +23,15 @@ class BaseDevis(models.Model):
     reference = models.CharField(max_length=100,blank=True,null=True)
     type_devis = models.CharField(max_length=10, choices=[('achat', 'Achat'), ('vente', 'Vente')])
     objet = models.CharField(max_length=100,blank=True,null=True)
+    devise = models.CharField(max_length=3, default='MAD', verbose_name="Devise",blank=True, null=True)
+    
     commission_pourcentage = models.DecimalField(max_digits=5, decimal_places=2, default=0.0,null=True, blank=True)
     remise = models.ForeignKey(Remise, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    condition_reglement= models.ForeignKey(ConditionsReglement, on_delete=models.SET_NULL, null=True, blank=True)
+    moyen_de_paiement = models.CharField(max_length=12, choices=[('Espèce', 'Espèce'), ('Prélèvement', 'Prélèvement'),('Chèque', 'Chèque'), ('Virement', 'Virement'),('Versement', 'Versement'),('Autre','Autre')],default='Autre')
     adresse_de_livraison = models.CharField(max_length=100,blank=True,null=True)
-    delai_livraison = models.CharField(max_length=100,blank=True,null=True)
+    date_livraison = models.DateField(blank=True,null=True)
     condition_livraison = models.CharField(max_length=100,blank=True,null=True)
     note=models.TextField()
     
@@ -71,7 +76,30 @@ class Devis(BaseDevis):
     
 
 class LigneDevis(BaseLigne):
+    
     devis = models.ForeignKey(Devis, on_delete=models.CASCADE, related_name='lignes_devis')
+    reference= models.CharField(max_length=50, unique=True,blank=True, null=True)
+    product = models.ForeignKey(Produit, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    unit_price_ht = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=('Prix HT'))
+    unit_price_ttc = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=('Prix TTC'))
+    total_ht = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=('Total HT'), blank=True, null=True)
+    total_ttc = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=('Total TTC'), blank=True, null=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.unit_price_ht:
+            # Set unit_price_ht to the product's price_ht if not provided
+            self.unit_price_ht = self.product.price_ht
+
+        # Calculate total_ht and total_ttc
+        self.total_ht = self.quantity * self.unit_price_ht
+        self.total_ttc = self.quantity * self.unit_price_ttc
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.product.name} - {self.quantity}'
+    
     
 class Commande(BaseDevis):
     
