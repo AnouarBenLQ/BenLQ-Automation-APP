978eb7b830b157c22e3c3bd2d78efd82cf2a1762
Anouar <a.benlaqlib.com>
2024-03-27 02:06:32 +0000

    refactored invoice

--- transactions/forms.py
+++ transactions/forms.py
@@ -4,6 +4,7 @@ from contacts.models import Client
 from coreApp.models import Entreprise
 from datetime import date,datetime, timedelta
 from django import forms
+from accounts.models import CustomUser
 
 
 class DevisForm(forms.ModelForm):
@@ -26,11 +27,14 @@ class InformationDevis(forms.ModelForm):
         # Set initial values for date and echeance
         self.fields['date'].initial = date.today()
         self.fields['echeance'].initial = date.today() + timedelta(days=30)
-
-    date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), label='Date')
-    echeance = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), label='Echeance')
-
+    client = forms.ModelChoiceField(queryset=Client.objects.all(), label='Client',
+                                    widget=forms.Select(attrs={'class': 'w-1/2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'}),)
+    date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date','class':'rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'}), label='Date')
+    echeance = forms.DateField(widget=forms.DateInput(attrs={'type': 'date','class':'rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'}), label="Valable jusqu'au")
+    reference = forms.CharField(widget=forms.DateInput(attrs={'type': 'text','class':'rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'}), label="Réference")
+    suivi_par = forms.ModelChoiceField(queryset=CustomUser.objects.all(), label='Suivi Par',
+                                    widget=forms.Select(attrs={'class': 'w-1/2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'}),)
     class Meta:
         model = Devis
-        fields = ['numero', 'suivi_par', 'client', 'commercial', 'date', 'echeance']
+        fields = ['numero', 'suivi_par', 'client','reference', 'commercial', 'date', 'echeance',]
         
